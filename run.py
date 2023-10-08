b66d1c3a5432ee7fff5a89bc878064479cbb32b8
JoannaAdermark1 Adermark <joanna.adermark@tutanota.com>
2023-10-08 10:15:50 +0000

    first commit

--- run.py
+++ run.py
@@ -1,7 +1,33 @@
 name = input('Enter your name! ')
 print(f'Greetings {name}!')
-print("welcaome to The Dragon Room Game! ")
-print("You find yourself in front of two doors. Do you go through door #1 or door #2?")
-print(" It is said that deep within the cavern, a dragon guards a priceless treasure, ")
-print("but those who enter must face a series of challenges and make crucial decisions")
-print("Your heart beats faster as you decide whether to enter.")
+
+def main():
+    print("You find yourself at the entrance of a dark cavern: the Dragon Room.")
+    print("Do you wish to enter the Dragon Room?")
+    choice = input("(Enter/Not Enter): ")
+
+    if choice == "Enter":
+        first_chamber( )
+    else:
+        end_game("You remain outside, forever wondering what adventures lay within the Dragon Room.")
+
+def first_chamber():
+    print("A flickering torch reveals three distinct paths: Gold, Darkness, Cries.")
+    choice = input("Which path do you choose? (Gold/Darkness/Cries): ")
+
+    if choice == "Gold":
+        gold_path(  )
+    elif choice == "Darkness":
+        darkness_path( ) 
+    elif choice == "Cries":
+        cries_path( )
+
+def gold_path():
+    print("You follow the glittering path and fall into a pit.")
+    choice = input("Do you give up some gold for a rope? (Yes/No): ")
+
+    if choice == "Yes":
+        dragon_door(.  )
+    else:
+        end_game("Your weight from the gold pulls you further down, and you're trapped.")
+
